34eef5e7401c0d906189de7b881ff0ee7a027c41
Inabass <prf0927@gmail.com>
2020-08-22 02:53:05 +0900

    go-fmt

--- src/main.go
+++ src/main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    _ "image/jpeg"
-    _ "image/png"
-    "os"
+	"fmt"
+	_ "image/jpeg"
+	_ "image/png"
+	"io/ioutil"
+	"os"
 )
 
-func getImg()([]byte, error) {
-    img, err := ioutil.ReadFile("resource/input/example.jpg")
+func getImg() ([]byte, error) {
+	img, err := ioutil.ReadFile("resource/input/example.jpg")
 
-    return img, err
+	return img, err
 }
 
 func writeImg(img []byte) {
-    err := ioutil.WriteFile("resource/output/example.jpg", img, 0644)
-    if err != nil {
-             fmt.Fprintln(os.Stderr, err)
-             return
-    }
+	err := ioutil.WriteFile("resource/output/example.jpg", img, 0644)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
 }
 
 func main() {
 	img, err := getImg()
-	 if err != nil {
-         fmt.Fprintln(os.Stderr, err)
-         return
-     }
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
 	writeImg(img)
 }
