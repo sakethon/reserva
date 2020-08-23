5b706129d12f17ce9ec26edc8e175b650112e703
rymiyamoto <miyamoto@sencorp.co.jp>
2020-08-23 22:13:22 +0900

    ロガーを定義して使えるように

--- src/main.go
+++ src/main.go
@@ -6,7 +6,27 @@ import (
 	_ "image/png"
 	"io/ioutil"
 	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+func createLogger() (*zap.Logger, error) {
+	logConfig := zap.Config{
+		OutputPaths: []string{"stdout"},
+		Level:       zap.NewAtomicLevelAt(zapcore.InfoLevel),
+		Encoding:    "json",
+		EncoderConfig: zapcore.EncoderConfig{
+			LevelKey:    "level",
+			TimeKey:     "time",
+			MessageKey:  "msg",
+			EncodeTime:  zapcore.ISO8601TimeEncoder,
+			EncodeLevel: zapcore.LowercaseLevelEncoder,
+		},
+	}
+
+	return logConfig.Build()
+}
 
 func getImg() ([]byte, error) {
 	img, err := ioutil.ReadFile("resource/input/example.jpg")
@@ -23,6 +43,11 @@ func writeImg(img []byte) {
 }
 
 func main() {
+	logger, err := createLogger()
+	if err != nil {
+		return
+	}
+
 	img, err := getImg()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
